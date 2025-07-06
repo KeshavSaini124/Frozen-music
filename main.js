4879ef50a695d810df64911fd708ba717ce47659
KeshavSaini124 <101712695+KeshavSaini124@users.noreply.github.com>
2025-07-06 17:09:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 });
 
 // Add your bot token here.
-const BOT_TOKEN = "your bot token here";
+const BOT_TOKEN = "8034682967:AAEGanKE9BUXIMvNpe4F7VvwX2VBlaOiumM";
 
 //Pro people: don't change this else your bot will be fucked.
 const API_BASE_URL = "https://metropolitan-loise-vibeshiftbot-f38d853c.koyeb.app";
